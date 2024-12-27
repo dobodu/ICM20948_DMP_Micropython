f31619cb85f9f8a91e1e617fe09e9c47260b6177
dobodu <ludovic.boldu@sfr.fr>
2024-12-27 23:36:32 +0100

    Update icm20948_test.py
    Revision since the library has evolved (compass, gyroscope or accelerometer can be accessed the same way, DMP enable or not)

--- icm20948_test.py
+++ icm20948_test.py
@@ -4,8 +4,8 @@ from math import degrees, atan2
 from utime import sleep_ms
 from ustruct import unpack_from
 
-I2C0_SCL = Pin(07) # I2C SCL 
-I2C0_SDA = Pin(06) # I2C SCA
+I2C0_SCL = Pin(07)
+I2C0_SDA = Pin(06)
     
 i2c0 = I2C(0, sda=I2C0_SDA, scl=I2C0_SCL, freq = 400_000)
 
@@ -16,35 +16,13 @@ choice = input("Shall we use DMP (y/n) ? ")
 
 imu = icm20948.ICM20948(i2c0, dmp = True if (choice=="y") else False, debug=0b01001)
 
-if not imu.dmp_ready :
+if imu.dmp_ready :
     
-    imu.gyro_cal()
-
-    while True:
-        a = imu.acc()
-        g = imu.gyro()
-        m = imu.mag()   # Read directly to the slave
-        imu.Q_update_full(m, a, g)
-        heading = imu.heading
-        pitch = imu.pitch
-        roll = imu.roll
-
-        print(f"""
-Accel: {a[0]:05.2f} {a[1]:05.2f} {a[2]:05.2f}
-Gyro:  {g[0]:05.2f} {g[1]:05.2f} {g[2]:05.2f}
-Mag:   {m[0]:05.2f} {m[1]:05.2f} {m[2]:05.2f}
-HPT:   {heading:7.3f} {pitch:7.3f} {roll:7.3f}
-""")
-        
-        sleep_ms(25)
-    
-else :
-           
-    #imu.DMP_enable_sensor("ACCELEROMETER",True)
-    #imu.DMP_enable_sensor("GYROSCOPE",True)
+    imu.DMP_enable_sensor("ACCELEROMETER",True)
+    imu.DMP_enable_sensor("GYROSCOPE",True)
+    imu.DMP_enable_sensor("MAGNETIC_FIELD_UNCALIBRATED",True)
     #imu.DMP_enable_sensor("RAW_ACCELEROMETER",True)
     #imu.DMP_enable_sensor("RAW_GYROSCOPE",True)
-    imu.DMP_enable_sensor("MAGNETIC_FIELD_UNCALIBRATED",True)
     #imu.DMP_enable_sensor("GYROSCOPE_UNCALIBRATED",True)
     #imu.DMP_enable_sensor("ACTIVITY_CLASSIFICATON",True)
     #imu.DMP_enable_sensor("STEP_DETECTOR",True)
@@ -57,5 +35,24 @@ else :
     #imu.DMP_enable_sensor("LINEAR_ACCELERATION",True)
     #imu.DMP_enable_sensor("ORIENTATION",True)
     
-    while True:
-        imu.DMP_fifo_read()
+#imu.gyro_cal()
+#imu.acc_cal()
+#imu.mag_cal()
+
+while True:
+    a = imu.acc()
+    g = imu.gyro()
+    m = imu.mag()
+    imu.Q_update_full(m, a, g)
+    heading = imu.heading
+    pitch = imu.pitch
+    roll = imu.roll
+
+    print(f"""
+Accel: {a[0]:05.2f} {a[1]:05.2f} {a[2]:05.2f}
+Gyro:  {g[0]:05.2f} {g[1]:05.2f} {g[2]:05.2f}
+Mag:   {m[0]:05.2f} {m[1]:05.2f} {m[2]:05.2f}
+HPT:   {heading:7.3f} {pitch:7.3f} {roll:7.3f}
+""")
+        
+    sleep_ms(25)
